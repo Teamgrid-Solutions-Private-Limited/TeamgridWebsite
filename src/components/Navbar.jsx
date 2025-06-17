26decbbc22077f022fa41b22641d047dd4620e26
tgs-silpi <silpi.mandal@teamgrid.io>
2025-06-17 19:52:24 +0530

    Work on home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import styles from '../style/Navbar.module.css'
-import logo from "../assets/Layer_1.png";
+import React from "react";
+import styles from "../style/Navbar.module.css";
+import logo from "../assets/Layer_1.svg";
 import { FaAngleDown } from "react-icons/fa6";
 
 function Navbar() {
   return (
-    <div className={styles.mainBox}>
+    <nav className={styles.mainBox}>
       <div className={styles.innerBox}>
         <img src={logo} alt="logo" className={styles.logo} />
         <div className={styles.menu}>
@@ -16,9 +16,9 @@ function Navbar() {
             <li className={styles.menuItemBox}>
               <div className={styles.menuItem}>About Us</div>
             </li>
-            <li className={styles.menuItemBox} style={{display: "flex", alignItems: "center", justifyContent: "center", gap: "8px"}}>
+            <li className={`${styles.menuItemBox} ${styles.menuItemArrow}`}>
               <div className={styles.menuItem}>What We Do</div>
-              <FaAngleDown size={15} color="#0B3C7B" />
+              <FaAngleDown size={13} color="#0B3C7B" />
             </li>
             <li className={styles.menuItemBox}>
               <div className={styles.menuItem}>Technologies We Use</div>
@@ -27,7 +27,7 @@ function Navbar() {
               <div className={styles.menuItem}>How we work</div>
             </li>
             <li className={styles.menuItemBox}>
-              <div className={styles.menuItem}>Let’s Talk</div>
+              <div className={styles.menuItem}>Let's Talk</div>
             </li>
           </ul>
         </div>
@@ -35,8 +35,8 @@ function Navbar() {
           <div className={styles.buttonText}>Get a Quote</div>
         </div>
       </div>
-    </div>
+    </nav>
   );
 }
 
-export default Navbar
+export default Navbar;
