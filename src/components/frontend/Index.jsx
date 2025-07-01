89672ea99e08622f3a3c67dd48e806efef604a1f
Silpi Mandal <silpimandal08@gmail.com>
2025-07-01 13:48:39 +0530

    Add HomePage for frontend

---
+++ src/components/frontend/Index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "@mui/material";
+import HomePage from "./HomePage";
+
+function Index() {
+  return (
+    <Box>
+      <HomePage />
+    </Box>
+  );
+}
+
+export default Index;
